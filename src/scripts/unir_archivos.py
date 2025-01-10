0f069746d136ca9b19c981c343f31eef6bbec591
edcometlab <etorr27@lsu.edu>
2025-01-10 11:07:30 -0600

    un comment the download files

--- src/scripts/unir_archivos.py
+++ src/scripts/unir_archivos.py
@@ -30,28 +30,28 @@ def unir_archivos_grib(lista_archivos, salida="era5_2m_temperature_union.nc"):
             ds.close()
 
 if __name__ == "__main__":
-    ##### tmp
-    #ruta_datos = "../../data/raw/era5"
-    #ruta_datos_salida = "../../data/raw/processed"
-    #files = os.listdir(ruta_datos)
+    #### tmp
+    ruta_datos = "../../data/raw/era5"
+    ruta_datos_salida = "../../data/raw/processed"
+    files = os.listdir(ruta_datos)
 
-    ## Filter files that start with era5_2m_temperature
-    #files = [file for file in files if file.startswith("era5_tmp_")]
+    # Filter files that start with era5_2m_temperature
+    files = [file for file in files if file.startswith("era5_tmp_")]
 
-    ## Files that ends with .grib
-    #files = [file for file in files if file.endswith(".grib")]
+    # Files that ends with .grib
+    files = [file for file in files if file.endswith(".grib")]
 
-    ## Sort files
-    #files = sorted(files)
+    # Sort files
+    files = sorted(files)
 
-    ## Create a list with the full path of the files
-    #archivos = [os.path.join(ruta_datos, file) for file in files]
+    # Create a list with the full path of the files
+    archivos = [os.path.join(ruta_datos, file) for file in files]
 
-    #archivo_union = unir_archivos_grib(archivos, salida= os.path.join(ruta_datos_salida, "era5_tmp_union.nc"))
+    archivo_union = unir_archivos_grib(archivos, salida= os.path.join(ruta_datos_salida, "era5_tmp_union.nc"))
     
     ########################## rain
     ruta_datos = "../../data/raw/era5"
-    ruta_datos_salida = "../../data/processed"
+    ruta_datos_salida = "../../data/raw/processed"
     files = os.listdir(ruta_datos)
 
     # Filter files that start with era5_2m_temperature
